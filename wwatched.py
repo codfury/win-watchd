2ae9595523207903c23ee9a4e6160522ab432d78
codfury <shivam04252@gmail.com>
2022-04-30 16:42:46 +0530

    removed some unecessary comments

--- wwatched.py
+++ wwatched.py
@@ -13,19 +13,20 @@ from queue import Queue
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-log_stream = StringIO() 
 
 class EventHandler(FileSystemEventHandler):
 	'''Watches a specific folder and raises events on_created and on_deleted'''
 		
 	def __init__(self) -> None:
 		super().__init__()
-
-		self.moved=False
-		self.output_filename=r'../logs.csv'
+	
 
 	
 	def log_csv(self,timestamp,event,src='',dest=''):
+		'''Logs events in csv as they occur.
+		:param: timestamp,event,src='',dest=''
+		:return:
+		'''
 		src=str(src)
 		dest=str(dest)
 		data=[timestamp,event,src,dest]
@@ -34,89 +35,64 @@ class EventHandler(FileSystemEventHandler):
 			csv_writer = writer(write_obj) 
 			csv_writer.writerow(data)
 
-		
+	def on_moved(self, event):
+		'''Handles on_moved event. Just notes to csv when a file/folder is Renamed.
+		:param: event
+		:return:
+		'''
+		super().on_moved(event)
+		if not event.is_directory:
+			self.log_csv(str(datetime.now()),'Renamed File',event.src_path,event.dest_path)
+			#logging.info("Renamed file: {}".format(event.src_path))
+		else:
+			self.log_csv(str(datetime.now()),'Renamed Folder',event.src_path,event.dest_path)
+			#logging.info("Renamed Folder: {}".format(event.src_path))
 
-	# def on_any_event(self, event):
-	# 	super().on_any_event(event)	
-	# 	#logging.info("New file: {}".format(event.src_path))
-	# 	logging.info(event)
-
-
-
-	# def on_moved(self, event):
-	# 	'''
-	# 	Handles on_moved/renamed event.Just notes to csv when a on_moved/renamed is created.
-	# 	:param event:
-	# 	:return:
-	# 	'''
-	# 	try:
-	# 		super().on_moved(event)
-	# 	except TypeError:
-	# 		pass
-	# 	finally:
-	# 		src=	list(str(event.src_path).split('//'))
-	# 		dest=list(str(event.dest_path).split('//'))
-	# 		self.on_moved=True
-	# 		if(''.join(src)[:len(src)-1]==''.join(dest)[:len(src)-1]):
-	# 			self.log_csv(datetime.now(),'Renamed File',event.src_path)
-
-			
-	# 			logging.info("Renamed: {}".format(event.src_path))
-	# 		else:
-	# 			self.log_csv(str(datetime.now()),'Moved File',event.src_path,event.dest_path)
-	# 			logging.info("Moved to another folder: {}".format(event.src_path))
-
-
-
-	
 	def on_created(self, event):
 		'''
 		Handles on_created event. Just notes to csv when a file/folder is created.
-		:param event:
+		:param: event
 		:return:
 		'''
 		super().on_created(event)
 		if not event.is_directory:
 			self.log_csv(str(datetime.now()),'Created File',event.src_path)
-			logging.info("New file: {}".format(event.src_path))
+			#logging.info("New file: {}".format(event.src_path))
 		else:
 			self.log_csv(str(datetime.now()),'Created Folder',event.src_path)
-			logging.info("New folder: {}".format(event.src_path))
+			#logging.info("New folder: {}".format(event.src_path))
 
 
 
 	def on_deleted(self, event):
 		'''
 		Handles on_deleted event. Just notes to logger when a file/folder is deleted.
-		:param event:
+		:param: event
 		:return:
 		'''
 		super().on_deleted(event)
 		if not event.is_directory:
-			logging.info("Deleted file: %s", event.src_path)
+			#logging.info("Deleted file: %s", event.src_path)
 			self.log_csv(str(datetime.now()),'Deleted File',event.src_path)
 		else:
-			logging.info("Deleted folder: %s", event.src_path)
+			#logging.info("Deleted folder: %s", event.src_path)
 			self.log_csv(str(datetime.now()),'Deleted Folder',event.src_path)
 
 	
 	def on_modified(self, event):
 		'''
 		Handles on_modified event. Just notes to logger when a file/folder is modified.
-		:param event:
+		:param: event
 		:return:
 		'''
 		super().on_modified(event)
-		# if(self.on_moved):
-		# 	self.on_moved=False
-		# 	return
 		
 		if not event.is_directory:
 		
-			logging.info("Modified file: %s", event.src_path)
+			#logging.info("Modified file: %s", event.src_path)
 			self.log_csv(str(datetime.now()),'Modified File',event.src_path)
 		else:
-			hel=logging.info("Modified folder: %s", event.src_path)
+			#logging.info("Modified folder: %s", event.src_path)
 			self.log_csv(str(datetime.now()),'Modified Folder',event.src_path)
 			
 
@@ -128,7 +104,7 @@ if __name__ == "__main__":
 
 	with open(r'./logs.csv', "w") as write_obj: 
 		csv_writer = writer(write_obj)
-		csv_writer.writerow(['Timestamp(+5:30GMT)','Event Type','SourceLocation','NewLocation']) 
+		csv_writer.writerow(['Timestamp','Event Type','PathLocation','Renamed']) 
 		write_obj.close()
 	path = r"C:\Users\ACER\Dev\disecto\watched"
 	event_handler = EventHandler()
